b5553593bb3fd12e3383bb46cfa9d18a847ed6f3
gtt <1142489770@qq.com>
2018-11-27 09:52:09 +0800

    创建登录页面

---
+++ src/components/login/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login_wrap">
+    <el-form label-position="top" label-width="80px" :model="formdata" class="login_form">
+        <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.name"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+       <el-button type="primary" class="login_btn">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        formdata:{
+            name:'',
+            password:''
+        }
+    }
+}
+}
+</script>
+
+<style>
+.login_wrap{
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login_wrap .login_form{
+    width: 400px;
+    background-color: #fff;
+    padding: 30px;
+
+}
+ .login_wrap .login_btn{
+    width: 100%;
+}
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import  reset from '@/assets/css/reset.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
