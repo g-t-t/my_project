dd0f43891e3ab73314575fa439acb85bf0d1b386
gtt <1142489770@qq.com>
2018-11-27 22:14:15 +0800

    实现首页布局和退出功能

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -15,7 +15,7 @@
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-            <a href="#" class="loginOut">退出</a>
+            <a href="#" class="loginOut" @click="login_Out()">退出</a>
           </div>
         </el-col>
       </el-row>
@@ -23,92 +23,107 @@
 
     <!-- 侧边栏 -->
     <el-container>
-      <el-aside width="200px" class="aside">
+      <el-aside width="200px">
         <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
+          :router="true"
+          :unique-opened="true"
         >
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>用户管理</span>
             </template>
-            <el-menu-item>
+            <el-menu-item index="1-1">
               <i class="el-icon-menu"></i>
               <span>用户列表</span>
             </el-menu-item>
           </el-submenu>
 
           <!-- 2 -->
-          <el-submenu index="1">
+          <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>权限管理</span>
             </template>
-            <el-menu-item>
+            <el-menu-item index="2-1">
               <i class="el-icon-menu"></i>
               <span>角色列表</span>
             </el-menu-item>
-            <el-menu-item>
+            <el-menu-item index="2-2">
               <i class="el-icon-menu"></i>
               <span>权限列表</span>
             </el-menu-item>
           </el-submenu>
 
           <!-- 3 -->
-          <el-submenu index="1">
+          <el-submenu index="3">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>商品管理</span>
             </template>
-            <el-menu-item>
+            <el-menu-item index="3-1">
               <i class="el-icon-menu"></i>
               <span>商品列表</span>
             </el-menu-item>
-            <el-menu-item>
+            <el-menu-item index="3-2">
               <i class="el-icon-menu"></i>
               <span>分类参数</span>
             </el-menu-item>
-            <el-menu-item>
+            <el-menu-item index="3-3">
               <i class="el-icon-menu"></i>
               <span>商品分类</span>
             </el-menu-item>
           </el-submenu>
           <!-- 4 -->
-          <el-submenu index="1">
+          <el-submenu index="4">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>订单管理</span>
             </template>
-            <el-menu-item>
+            <el-menu-item index="4-1">
               <i class="el-icon-menu"></i>
               <span>订单列表</span>
             </el-menu-item>
           </el-submenu>
           <!-- 5 -->
-          <el-submenu index="1">
+          <el-submenu index="5">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>数据统计</span>
             </template>
-            <el-menu-item>
+            <el-menu-item index="5-1">
               <i class="el-icon-menu"></i>
               <span>数据报表</span>
             </el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
-      <!-- 中间 -->
-      <el-main class="main">Main</el-main>
-    </el-container>
+        <!-- 中间 -->
+   <el-main class="main">Main</el-main>
+</el-container>
   </el-container>
 </template>
 
 <script>
 export default {
+    // 钩子函数 newvue之前自动触发
+    beforeCreate(){
+        // 获取token
+        const token= localStorage.getItem('token')
+        if(!token){
+            // 如果没有token，先登陆
+            this.$router.push({name:'login'})
+        }
+    },
 
+methods:{
+    login_Out(){
+        // 清除token
+        localStorage.clear()
+        this.$message.success('退出成功')
+        this.$router.push({name:'login'})
+    }
+}
 }
 </script>
 
